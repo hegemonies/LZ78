eb440f61ec2ad39eae3e6b474263e92f58c14df0
hegemonies <hegemonies@yandex.ru>
2017-05-09 21:08:49 +0700

    Continuation of work on the algorithm

--- main.c
+++ main.c
@@ -1,44 +1,59 @@
 #include <stdio.h>
-
-typedef struct {
-	int n_str;
-	char ch;
-} Сode;
+#include <string.h>
 
 typedef struct {
 	int n_str;
 	char *str;
 } Dictionary;
 
-void init_dic(Dictionary **arr_dic, int n)
+void init_dic(Dictionary **arr_dic, int *n)
 {
-	for (int i = 0; i < n; i++) {
-		if (!arr_dic[i]) {
-			arr_dic[i] = malloc(sizeof(Dictionary*));	
-		}
+	if (!arr_dic[*n]) {
+		arr_dic[*n] = malloc(sizeof(Dictionary*));
+		*n++;
 	}
 }
 
 int main(int argc, char *argv[])
 {
+	/*
 	FILE *in = fopen(argv[4], "r");
 	FILE *out = fopen(argv[3], "w");
+	*/
+	FILE *in = fopen("war.txt", "r");
+	FILE *out = fopen("total.lz78", "w");
 
-	Code **arr_code = malloc(sizeof(Code*));
 	Dictionary **arr_dic = malloc(sizeof(Dictionary*));
+	int count_dic = 0;
 
-	init_dic(arr_dic, 1);
+	init_dic(arr_dic, &count_dic);
 
-	char ex[] = "kot kolom rubil slona";
-	
-	for (int i = 0; ex; i++) {
-		for (int j = 0; arr_dic[j]; j++) {
-			if (ex[i] != arr_dic[j]->str) {
-
+	//**
+	char *tmp = malloc(sizeof(char));
+	for (fread(tmp, 1, 1, in)) {
+		char *buf = malloc(sizeof(char) * 512);
+		if (!ferror(in)) {
+			for (int i = 0; arr_dic[i]; i++) {
+				if (arr_dic[i] && !strcmp(arr_dic[i]->str, tmp)) {
+					strcat(arr_dic[i]->str, tmp);
+					fread(tmp, 1, 1, in);
+					continue;
+				}
+				if (arr_dic[i] && ())
+				if (!arr_dic[i]) {
+					init_dic(arr_dic, 1);
+					arr_dic[i]->n_str = i;
+					arr_dic[i]->str = tmp;
+					//запись в структуру словаря
+				}
 			}
 		}
+		if (tmp) {
+			free(tmp);
+		}
+		
 	}
-	
+	///**
 	fclose(in);
 	fclose(out);
 	return 0;
