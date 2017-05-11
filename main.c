5655d9de6232e7ad56ebae6025f517d71b09dac0
hegemonies <hegemonies@yandex.ru>
2017-05-11 20:08:07 +0700

    Update

--- main.c
+++ main.c
@@ -1,60 +1,107 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct {
 	int n_str;
 	char *str;
 } Dictionary;
 
-void init_dic(Dictionary **arr_dic, int *n)
+void init_dic(Dictionary **arr_dic, int n)
 {
-	if (!arr_dic[*n]) {
-		arr_dic[*n] = malloc(sizeof(Dictionary*));
-		*n++;
+	arr_dic = realloc(arr_dic, n);
+	if (!arr_dic) {
+		return;
 	}
+	arr_dic[n] = malloc(sizeof(Dictionary*));
 }
 
 int main(int argc, char *argv[])
 {
-	/*
-	FILE *in = fopen(argv[4], "r");
-	FILE *out = fopen(argv[3], "w");
-	*/
-	FILE *in = fopen("war.txt", "r");
-	FILE *out = fopen("total.lz78", "w");
+	
+	//FILE *in = fopen(argv[4], "r");
+	//FILE *out = fopen(argv[3], "w");
+	
+	FILE *file_in = fopen("war.txt", "r");
+	FILE *file_out = fopen("total.lz78", "w");
 
 	Dictionary **arr_dic = malloc(sizeof(Dictionary*));
-	int count_dic = 0;
 
-	init_dic(arr_dic, &count_dic);
+	//init_dic(arr_dic, 1);
 
-	//**
-	char *tmp = malloc(sizeof(char));
-	for (fread(tmp, 1, 1, in)) {
-		char *buf = malloc(sizeof(char) * 512);
-		if (!ferror(in)) {
-			for (int i = 0; arr_dic[i]; i++) {
-				if (arr_dic[i] && !strcmp(arr_dic[i]->str, tmp)) {
-					strcat(arr_dic[i]->str, tmp);
-					fread(tmp, 1, 1, in);
-					continue;
-				}
-				if (arr_dic[i] && ())
-				if (!arr_dic[i]) {
-					init_dic(arr_dic, 1);
-					arr_dic[i]->n_str = i;
-					arr_dic[i]->str = tmp;
-					//запись в структуру словаря
-				}
-			}
+	/*
+	char *tmp = malloc(sizeof(char) * 10);
+	char *buf = malloc(sizeof(char) * 512);
+	int t;
+	for (int i = 0; ((t = fread(tmp, 1, 1, file_in)) && !ferror(file_in)); i++) {
+		//if (!ferror(file_in)) {
+		//buf = tmp;
+		strcat(buf, tmp);
+		if (!arr_dic[i]) {
+			//init_dic(arr_dic, i);
+			arr_dic[i] = malloc(sizeof(Dictionary*));
+			arr_dic[i]->n_str = i;
+			arr_dic[i]->str = buf;
+			//i--;
+			free(buf);
+			//buf = NULL;
+			continue;
 		}
-		if (tmp) {
-			free(tmp);
+		if (arr_dic[i] && !strcmp(arr_dic[i]->str, buf)) {
+			strcat(buf, tmp);
+			t = fread(tmp, 1, 1, file_in);
+
 		}
 		
+			
+			for (int i = 0; arr_dic[i]; i++) {
+				char *buf = malloc(sizeof(char) * 512);
+				buf = tmp;
+				if (!arr_dic[i]) {
+					init_dic(arr_dic, i);
+					arr_dic[i]->n_str = i;
+					arr_dic[i]->str = buf;
+					//i--;
+					free(buf);
+					continue;
+				}
+				if (arr_dic[i] && !strcmp(arr_dic[i]->str, buf)) {
+					strcat(buf, tmp);
+					fread(tmp, 1, 1, file_in);
+				}
+			}
+			
+		//}
+	
 	}
-	///**
-	fclose(in);
-	fclose(out);
+	for (int i = 0; arr_dic[i]; i++) {
+		printf("%d :: %s\n", arr_dic[i]->n_str, arr_dic[i]->str);
+	}
+	*/
+
+	char *tmp = malloc(sizeof(char));
+
+	arr_dic[0]->n_str = 0;
+	arr_dic[0]->str = "";
+con:
+	for (int i = 0; (fread(tmp, 1, 1, file_in) && !ferror(file_in)); i++) {
+		char *buf = malloc(sizeof(char) * 512);
+		strcat(buf, tmp);
+		if (!arr_dic[i + 1]) {
+			arr_dic[i + 1] = malloc(sizeof(Dictionary*));
+			arr_dic[i + 1]->n_str = i + 1;
+			arr_dic[i + 1]->str = buf;
+			continue;
+		}
+		if (arr_dic[i] && (strspn(arr_dic[i]->str, buf) == strlen(arr_dic[i]->str))) {
+			strcat(buf, tmp);
+			continue;
+		} else if (strspn(arr_dic[i]->str, buf) < strlen(arr_dic[i]->str))
+	}
+
+
+
+	fclose(file_in);
+	fclose(file_out);
 	return 0;
 }
