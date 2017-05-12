8580bfab057a3da1766cce3e74332602ed8dfeed
hegemonies <hegemonies@yandex.ru>
2017-05-12 22:00:40 +0700

    Update

--- test/test.c
+++ test/test.c
@@ -12,7 +12,7 @@ int main(void)
 	FILE *in = fopen("war.txt", "r");
 
 	//Dictionary *arr[10];
-	Dictionary **arr = malloc(sizeof(Dictionary) * 50);
+	Dictionary **arr = malloc(sizeof(Dictionary) * 20);
 	arr[0] = malloc(sizeof(Dictionary));
 	arr[0]->n_str = 0;
 	arr[0]->str = '\0';
@@ -26,7 +26,7 @@ int main(void)
 		Dictionary dic_tmp;
 		dic_tmp.n_str = 0;
 		dic_tmp.str = tmp;
-		printf("%d\n", tmp);
+		//printf("%d\n", tmp);
 		for (int i = 0;; i++) {
 			if (!arr[i + 1]) {
 				arr[i + 1] = malloc(sizeof(Dictionary));
@@ -37,7 +37,7 @@ int main(void)
 			}
 			if (dic_tmp.str == arr[i + 1]->str && dic_tmp.n_str == arr[i + 1]->n_str) {
 				dic_tmp.n_str = i + 1;
-				printf("%d\n", tmp);
+				//printf("%d\n", tmp);
 				fread(&tmp, 1, 1, in);
 				if (feof(in)) {
 					*arr[i + 2] = dic_tmp;
@@ -53,7 +53,7 @@ int main(void)
 
 	FILE *out = fopen("total.txt", "w");
 	for (int i = 0; arr[i]; i++) {
-		fprintf(out, "%d,'%c'|", arr[i]->n_str, arr[i]->str);
+		fprintf(out, "%d,%c|", arr[i]->n_str, arr[i]->str);
 	}
 	fclose(out);
 	fclose(in);
