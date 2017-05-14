1fc4810c8bb9053c6323be101bcb2c1eb86b4fbd
hegemonies <hegemonies@yandex.ru>
2017-05-14 21:26:22 +0700

    Update

--- src/main.c
+++ src/main.c
@@ -5,29 +5,8 @@
 #include "coder.h"
 #include "strings.h"
 
-int main(void)
+void print_dic_and_code(Dictionary dic, Code *code)
 {
-	FILE *in = fopen("txt/war.txt", "r");
-
-	Dictionary dic;
-	dic_init(&dic);
-
-	Code *code = calloc(dic.capacity, sizeof(Code));
-	code_init(code);
-	
-/*--------------------------COMPRES-----------------------------*/
-	compres(&dic, code, in);
-/*--------------------DECOMPRES---------------------------------*/
-	/*
-	Dictionary dic_t;
-	dic_init(*dic_t);
-	*/
-
-
-
-/*---------------------------------------------------------------*/
-
-	//FILE *out = fopen("total.lz78", "w");
 	printf("Dictionary:\n");
 	for (int i = 0; i < dic.size; i++) {
 		printf("%d, '%s'\n", i, dic.dic_i[i].str);
@@ -37,6 +16,51 @@ int main(void)
 	for (int i = 0; i < dic.size; i++) {
 		printf("%d, %c\n", code[i].num, code[i].str);
 	}
+}
+
+int main(void)
+{
+	FILE *in = fopen("txt/war.txt", "r");
+
+/*--------------------------CREATE TO DICTIONARY----------------*/
+	Dictionary dic;
+	dic_init(&dic);
+/*--------------------------CREATE TO CODE----------------------*/
+	Code *code = calloc(dic.capacity, sizeof(Code));
+	code_init(code);
+
+/*--------------------------COMPRES-----------------------------*/
+	compres(&dic, code, in);
+/*--------------------------DECOMPRES---------------------------*/
+	Dictionary dic_t;
+	dic_init(&dic_t);
+	for (int i = 0; code[i].str; i++) {// TODOOOOOO
+
+		if (!code[i].num) {
+			dic_t.dic_i[dic.size].str = calloc(1, sizeof(char));
+			char *buf = malloc(sizeof(char) + 1);
+			buf[1] = 0;
+			scat(buf, &code[i].str);
+			scat(dic_t.dic_i[dic.size].str, buf);
+			free(buf);
+			continue;
+		} else {
+			char *buf = malloc(sizeof(char) * slen(dic_t.dic_i[code[i].num].str) + 2);
+			scat(buf, dic_t.dic_i[code[i].num].str);
+			buf[slen(dic_t.dic_i[code[i].num].str)] = 0;
+			scat(buf, &code[i].str);
+			printf("%s\n", buf);
+			free(buf);
+		}
+	}
+
+
+
+/*---------------------------------------------------------------*/
+
+	//FILE *out = fopen("total.lz78", "w");
+	
+	//print_dic_and_code(dic, code);
 	
 	/* нафиг фри
 	for (int i = 1; i < dic.size; i++) {
